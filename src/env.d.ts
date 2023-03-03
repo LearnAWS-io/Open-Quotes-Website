aad66ccd4ef9c4b0eacb2de55d1fd00e965dd3f9
Shivam <singhshivamkr@gmail.com>
2023-03-03 23:25:39 +0530

    :label: Add table name in env type

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly TABLE_NAME: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
