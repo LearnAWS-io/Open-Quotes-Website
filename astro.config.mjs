b45a610a60983ac77368fa94f7d69acb6716abfa
Shivam <singhshivamkr@gmail.com>
2023-03-04 16:14:24 +0530

    :wrench: Add astro aws adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
-
+import { astroAWSFunctions } from "@astro-aws/adapter";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: astroAWSFunctions(),
+  vite: { esbuild: { exclude: ["@aws-sdk/*"] } },
 });
