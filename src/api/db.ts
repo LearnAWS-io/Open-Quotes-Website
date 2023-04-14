a5baae5709e39ce510328327d178f8ed424db370
Shivam <singhshivamkr@gmail.com>
2023-04-14 16:29:21 +0530

    :fire: remove type from db client

--- src/api/db.ts
+++ src/api/db.ts
@@ -1,10 +1,3 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
-export type BaseTable = {
-  GSI1PK: string;
-  GSI1SK: string;
-  PK: string;
-  SK: string;
-};
-
 export const dbClient = new DynamoDBClient({ region: "us-east-1" });
